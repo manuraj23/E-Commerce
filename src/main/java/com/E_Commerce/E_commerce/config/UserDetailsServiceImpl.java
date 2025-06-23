7ce3ec1f96d7fa6d82d9155eb9db243d30ecbea5
manuraj23 <manuraj082004@gmail.com>
2025-06-23 09:36:05 +0530

    updated error

--- src/main/java/com/E_Commerce/E_commerce/config/UserDetailsServiceImpl.java
+++ src/main/java/com/E_Commerce/E_commerce/config/UserDetailsServiceImpl.java
@@ -10,7 +10,7 @@ import com.E_Commerce.E_commerce.model.UserDtls;
 import com.E_Commerce.E_commerce.repository.UserRepository;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class  UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
 	private UserRepository userRepository;
--- src/main/java/com/E_Commerce/E_commerce/util/CommonUtil.java
+++ src/main/java/com/E_Commerce/E_commerce/util/CommonUtil.java
@@ -30,7 +30,7 @@ public class CommonUtil {
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message);
 
-		helper.setFrom("daspabitra55@gmail.com", "Shooping Cart");
+		helper.setFrom("manuraj0642@gmail.com", "Shooping Cart");
 		helper.setTo(reciepentEmail);
 
 		String content = "<p>Hello,</p>" + "<p>You have requested to reset your password.</p>"
